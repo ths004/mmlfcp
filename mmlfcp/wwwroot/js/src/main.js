eb8517d5c5b0441a2a0580de9f02c1b29765476f
yeonkyung <ygk1204@naver.com>
2026-03-25 16:24:00 +0900

    #34 토큰 뒤 &path 없을시 생손보(lifefire)로 기본 설정

--- mmlfcp/wwwroot/js/src/main.js
+++ mmlfcp/wwwroot/js/src/main.js
@@ -36,9 +36,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         mmlfcp_state.set('ga_id', authResult.ga_id);
         mmlfcp_state.set('upload_date', authResult.upload_date);
 
-        //console.log({ upload_date: authResult.upload_date });
-
-
         // ⭐ 추가: URL에서 받은 path 값을 state에 저장
         if (path) {
             mmlfcp_state.set('url_path', path);
